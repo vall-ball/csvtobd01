4cae654d5682884caa94402fb953b24546f9e6f0
vall-ball <vall-ball@yandex.ru>
2019-09-09 13:37:50 +0700

    Second Commit

--- src/main/java/ru/vallball/csvtobd01/Color.java
+++ src/main/java/ru/vallball/csvtobd01/Color.java
@@ -5,28 +5,34 @@ import javax.persistence.Id;
 
 @Entity
 public class Color {
-	
+
 	public Color() {
-	
+
 	}
+
 	public Color(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
+
 	@Id
 	private int id;
 	private String name;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 }
